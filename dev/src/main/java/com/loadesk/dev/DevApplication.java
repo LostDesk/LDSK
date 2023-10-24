7988c617907bd7f40a2f51924ac2182f1d084992
Blen <blenbre777@naver.com>
2023-10-24 21:46:26 +0900

    properties 및 db setting

--- dev/src/main/java/com/loadesk/dev/DevApplication.java
+++ dev/src/main/java/com/loadesk/dev/DevApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.mvc"}) // Class들을 자동으로 스캔하여 Bean 으로 등록해주는 package 경로 
 public class DevApplication {
 
 	public static void main(String[] args) {
