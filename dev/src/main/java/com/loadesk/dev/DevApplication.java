0f4b3627daed19cf26e439fe58ae6d9268e80157
이홍재 <dlghdwo5153@gmail.com>
2023-10-29 22:12:21 +0900

    config, filter, provider, properties 추가

--- dev/src/main/java/com/loadesk/dev/DevApplication.java
+++ dev/src/main/java/com/loadesk/dev/DevApplication.java
@@ -2,6 +2,7 @@ package com.loadesk.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"dev.mvc"}) // Class들을 자동으로 스캔하여 Bean 으로 등록해주는 package 경로 
