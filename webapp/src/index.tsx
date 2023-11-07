44302db270b7e3ae8146997fd9b7631bf1b71aba
이홍재 <dlghdwo5153@gmail.com>
2023-11-07 18:22:34 +0900

    커밋

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './views/mainpage/app.jsx';
+import App from '../src/App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+export {};
---
+++ webapp/src/types/interface/index.ts
@@ -0,0 +1,9 @@
+import BoardListItem from "./board-list-item.interface";
+import FavoriteListItem from "./favorite-list-item.interface";
+import CommentListItem from "./comment-list-item.interface";
+
+export type { // 인터페이스를 내보낼때 type 사용
+    BoardListItem,
+    FavoriteListItem,
+    CommentListItem
+}
